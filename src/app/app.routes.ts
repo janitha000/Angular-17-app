d84e72325fc99b701eecb4be442af9a8d1873488
Janitha Tennakoon <janitha@randomsoftware.net>
2024-04-03 09:10:33 +0530

    [ADD] Add boostrap with scss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PhotoGalleryComponent } from './photo-gallery/photo-gallery.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'photo-gallery',
+    component: PhotoGalleryComponent,
+  },
+];
